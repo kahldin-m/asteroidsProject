ee2df8f9fa977906ad0b4d8bb4aacce446ec16b2
kahldin-m <mglen.413@workmail.com>
2025-07-17 17:22:57 -0800

    B: Added Player (no input)

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import pygame
+import pygame # type: ignore
+from player import Player
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 
 def main():
@@ -6,15 +7,25 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game_clock = pygame.time.Clock()
+    dt = 0
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
+
+    # Game Loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                print("Closing... Goodbye!")
                 return
-        # pygame.Surface.fill("black")
-
+            
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        dt = game_clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
